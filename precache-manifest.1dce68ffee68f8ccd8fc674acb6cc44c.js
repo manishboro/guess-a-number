67d1df5961591d1ddc639028f48daa82d6abb14a
manishboro <officialmanishboro@gmail.com>
2020-04-11 22:34:25 +0530

    Updates

---
+++ precache-manifest.1dce68ffee68f8ccd8fc674acb6cc44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8732eb9c32cac7808dcf20d8420321",
+    "url": "/guess-a-number/index.html"
+  },
+  {
+    "revision": "00f5d88fb086047c0313",
+    "url": "/guess-a-number/static/css/main.a185f102.chunk.css"
+  },
+  {
+    "revision": "2363770714bd66a15cda",
+    "url": "/guess-a-number/static/js/2.af389951.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/guess-a-number/static/js/2.af389951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f5d88fb086047c0313",
+    "url": "/guess-a-number/static/js/main.143979f1.chunk.js"
+  },
+  {
+    "revision": "04e6777f6768210323a6",
+    "url": "/guess-a-number/static/js/runtime-main.a284c768.js"
+  }
+]);
